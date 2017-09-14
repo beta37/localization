5975c000a3630d60e1b2b6061d78613d88fc267e
Mikolay Bet <beta37@users.noreply.github.com>
2017-09-14 11:21:55 +0300

    New translations auth.php (Avestan)

---
+++ ja/auth.ae.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * auth.php
+ * Copyright (C) 2016 thegrumpydictator@gmail.com
+ *
+ * This software may be modified and distributed under the terms of the
+ * Creative Commons Attribution-ShareAlike 4.0 International License.
+ *
+ * See the LICENSE file for details.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'これらの資格が一致しない当社の記録です。',
+    'throttle' => '多くのログインを試みます。 て再度試してみてください：秒です。',
+    'testing field' => '試験分野の(変更)upd2'.
+
+];
