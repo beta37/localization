5a153961ba38905db322e3b9b35d6413cb6dcebd
Mikolay Bet <beta37@users.noreply.github.com>
2017-09-14 10:56:28 +0300

    New translations auth.php (Acholi)

--- en/auth.ach.php
+++ en/auth.ach.php
@@ -23,7 +23,7 @@ return [
     */
 
     'failed'   => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'throttle' => 'Sd.',
     'testing field' => 'sdsdd'.
 
 ];
